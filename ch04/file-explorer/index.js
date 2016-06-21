d1dbbc1d714d07e381e55e82ceff17db44dd4458
sundalei <sundalei2011@163.com>
2016-06-22 01:11:15 +0800

    201606211711

--- ch04/file-explorer/index.js
+++ ch04/file-explorer/index.js
@@ -12,7 +12,7 @@ fs.readdir(process.cwd(), function (err, files) {
   console.log('');
 
   if (!files.length) {
-    return console.log('    No files to show!\n');
+    return console.log('    \033[31m No files to show!\033[39m\n');
   }
 
   console.log('    Select which file or directory you want to see\n');
