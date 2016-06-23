a83c0a96a96508eef1aa2ff172851f2e0c998178
sundalei <sundalei2011@163.com>
2016-06-24 02:13:24 +0800

    201606231813

---
+++ ch05/web-server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type' : 'text/html'});
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(3000);
