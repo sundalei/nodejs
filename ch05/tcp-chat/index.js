8a8ce599ff245974d882f03321473f9c1f8cb118
sundalei <sundalei2011@163.com>
2016-06-24 18:18:44 +0800

    201606241818

--- ch05/tcp-chat/index.js
+++ ch05/tcp-chat/index.js
@@ -3,15 +3,35 @@
 /**
  * Module dependencies
  */
+
 var net = require('net');
+
+/**
+ * Keep track of connections.
+ */
+
+var count = 0;
 
 /**
  * Create server.
  */
 
-var server = net.createServer(function (conn) {
+var server = net.createServer(function (socket) {
+  socket.setEncoding('utf8');
   // handle connection
-  console.log('\033[90m  new connection!\033[39m');
+  socket.write('\n > welcome to \033[92mnode-chat\033[39m!'
+     + '\n > ' + count + ' other people are connected at this time.'
+     + '\n > please write your name and press enter: ');
+  count++;
+
+  socket.on('data', function (data) {
+    console.log(data);
+  });
+
+  socket.on('close', function () {
+    //console.log('close occurs');
+    count--;
+  })
 });
 
 /**
