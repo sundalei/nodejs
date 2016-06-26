039d947834a151039795da10757107b5e88017fb
sundalei <sundalei2011@163.com>
2016-06-26 11:08:21 +0800

    201606261108

---
+++ ch07/web-server3.js
@@ -0,0 +1,7 @@
+'use strict';
+
+require('http').createServer(function (req, res) {
+  console.log(req.headers);
+  res.writeHead(200, {'Content-Type' : 'text/html'});
+  res.end('Hello <b>World</b>');
+}).listen(3000);
