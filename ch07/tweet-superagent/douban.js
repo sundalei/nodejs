d6ed8a9bb4a9051ac41b50e83a1c8fbd3ef34821
sundalei <sundalei2011@163.com>
2016-06-30 20:53:11 +0800

    201606302053

--- ch07/tweet-superagent/douban.js
+++ ch07/tweet-superagent/douban.js
@@ -2,6 +2,10 @@
 
 var request = require('superagent');
 
-request.get('http://api.douban.com/v2/movie/search').send({tag : '小清新'}).end(function (res) {
-  console.log(res.body);
+request.get('http://api.douban.com/v2/movie/search').send({tag : '小清新'}).end(function (err, res) {
+  var obj = res.body;
+  obj.subjects.forEach(function (record) {
+    console.log(record.title);
+  });
+  //console.log(res.text);
 });
