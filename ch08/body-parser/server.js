c0594fb0537f067fdaf69a02dd0b0e7ae2e8f220
sundalei <sundalei2011@163.com>
2016-07-08 02:01:27 +0800

    201607071801

--- ch08/body-parser/server.js
+++ ch08/body-parser/server.js
@@ -4,7 +4,8 @@ var connect = require('connect'),
     http = require('http'),
     serveStatic = require('serve-static'),
     formidable = require('formidable'),
-    util = require('util');
+    util = require('util'),
+    fs = require('fs');
 
 var app = connect();
 
@@ -17,9 +18,23 @@ app.use(function (req, res, next) {
     form.uploadDir = __dirname + '/upload';
 
     form.parse(req, function (err, fields, files) {
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.write('received upload:\n\n');
-      res.end(util.inspect({fields: fields, files: files}));
+
+      fs.readFile(files.file.path, 'utf8', function (err, data) {
+        if (err) {
+          res.writeHead(500);
+          res.end('Error!');
+          return;
+        }
+
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end(
+          [
+            '<h3>File: ' + files.file.name + '</h3>',
+            '<h4>Type: ' + files.file.type + '</h4>',
+            '<h4>Contents:</h4><pre>' + data + '</pre>'
+          ].join('')
+        );
+      });
     });
   } else {
     next();
