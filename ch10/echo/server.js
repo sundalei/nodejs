4f500059fda137d069dc79e50daea2235f598999
sundalei <sundalei2011@163.com>
2016-07-18 22:17:46 +0800

    201607182217

--- ch10/echo/server.js
+++ ch10/echo/server.js
@@ -11,5 +11,18 @@ var server = app.listen(3000, function () {
 
 var wsServer = new WebSocketServer({
   httpServer : server,
-  
+  autoAcceptConnections : false
+});
+
+wsServer.on('request', function (request) {
+  var connection = request.accept('echo-protocol', request.origin);
+  console.log((new Date()) + ' Connection accepted.');
+  connection.on('message', function (message) {
+    console.log(' \033[96mgot:\033[39m ' + message.utf8Data);
+    connection.sendUTF('pong');
+  });
+
+  connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
 });
