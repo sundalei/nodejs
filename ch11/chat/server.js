e1d2f048b9cd004988fc08fe3914a48d638feb69
sundalei <sundalei2011@163.com>
2016-07-24 00:14:53 +0800

    201607240014

--- ch11/chat/server.js
+++ ch11/chat/server.js
@@ -24,7 +24,37 @@ var server = app.listen(3000, function () {
 
 //var io = sio.listen(server);
 var io = sio(server);
+var currentSong, dj;
+
+function elect (socket) {
+  dj = socket;
+  io.sockets.emit('announcement', socket.nickname + ' is the new dj');
+  socket.emit('elected');
+  socket.dj = true;
+  socket.on('disconnect', function () {
+    dj = null;
+    io.sockets.emit('announcement', 'the dj left - next one to join becomes dj');
+  });
+}
 
 io.on('connection', function (socket) {
-  console.log('Someone connected');
+  socket.on('join', function (name) {
+    socket.nickname = name;
+    socket.broadcast.emit('announcement', name + ' joined the chat.');
+
+    if (!dj) {
+      elect(socket);
+    } else {
+      socket.emit('song', currentSong);
+    }
+  });
+
+  socket.on('text', function (msg, fn) {
+    socket.broadcast.emit('text', socket.nickname, msg);
+
+    // confirm the reception
+    fn(Date.now());
+  });
 });
+
+// http://music.163.com/api/playlist/detail?id=428448345
