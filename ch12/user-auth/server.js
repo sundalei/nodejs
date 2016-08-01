be80bbc6b3959429889ce8c39bd4838ee8ce2acc
sundalei <sundalei2011@163.com>
2016-08-01 23:05:52 +0800

    201608012305

--- ch12/user-auth/server.js
+++ ch12/user-auth/server.js
@@ -20,6 +20,29 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({secret: 'my secret', resave: false, saveUninitialized: false}));
 
 /**
+ * Authentication middleware.
+ */
+var ObjectId = mongodb.ObjectID;
+app.use(function (req, res, next) {
+    if (req.session.loggedIn) {
+        // res.local('authenticated', true); //无效语法
+        res.locals.authenticated = true;
+        // console.log(req.session);
+        app.users.findOne({_id: ObjectId(req.session.loggedIn)}, function (err, doc) {
+            if(err) {
+                throw err;
+            }
+            res.locals.me = doc;
+            next();
+        });
+    } else {
+        // res.local('authenticated', false);
+        res.locals.authenticated = false;
+        next();
+    }
+});
+
+/**
  * Specify your views options.
  */
 
@@ -32,21 +55,39 @@ app.set('view engine', 'pug');
  * Default route
  */
 
-app.get('/', function(req, res) {
-    res.render('index', {authenticated: false});
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
 /**
  * Login route
  */
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
     res.render('login');
 });
 
 /**
  * Login process route
  */
+
+/**
+ * Login process route
+ */
+
+app.post('/login', function (req, res) {
+    app.users.findOne({email: req.body.email, password: req.body.password}, function (err, doc) {
+        if (err) {
+            return next(err);
+        }
+        if(!doc) {
+            return res.send('<p>User not found, Go back and try again</p>');
+        }
+        // console.log(doc._id.toString());
+        req.session.loggedIn = doc._id.toString();
+        res.redirect('/');
+    });
+});
 
 app.get('/login/:signupEmail', function (req, res) {
     res.render('login', {signupEmail: req.params.signupEmail});
@@ -75,6 +116,16 @@ app.post('/signup', function (req, res, next) {
 });
 
 /**
+ * Logout route
+ */
+
+app.get('/logout', function (req, res) {
+    // console.log(req.session);
+    req.session.loggedIn = null;
+    res.redirect('/');
+});
+
+/**
  * Connect to the database.
  */
 
@@ -91,6 +142,14 @@ MongoClient.connect(url, function(err, db) {
   
   // Get the users collection
   app.users = db.collection('users');
+  
+  // Create an index on the a field
+  app.users.createIndex({email: 1, password: 1}, function (err, indexName) {
+      if(err) {
+          throw err;
+      }
+      console.log('\033[96m + \033[39m ensured indexes');
+  });
 
   // db.close();
 });
