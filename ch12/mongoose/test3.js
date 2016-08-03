c9a9c0dcb15d718a6cec7e836267fb9bda664b36
sundalei <sundalei2011@163.com>
2016-08-03 22:56:02 +0800

    201608032256

--- ch12/mongoose/test3.js
+++ ch12/mongoose/test3.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 
 var CommentsSchema = new Schema({
     title: String,
-    body: String
+    body: String,
+    date: Date
 });
 
 var PostSchema = new Schema({
@@ -27,8 +28,10 @@ mongoose.connect('mongodb://localhost/test3');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-    var comments = [{title: 'comment1', body: 'body1'}, {title: 'comment2', body: 'body2'}];
-    new Post({_author: new mongoose.Types.ObjectId, title: 'My title', body: 'Welcome to my blog', comments: comments, meta: {
+    var comments = [{title: 'comment1', body: 'body1', date: Date('2016-08-02')}, 
+                    {title: 'comment2', body: 'body2', date: Date('2016-08-03')}];
+    new Post({_author: new mongoose.Types.ObjectId, title: 'My title', 
+              body: 'Welcome to my blog', comments: comments, meta: {
         votes: 5, favs : 15
     }}).save(function (err, blogposts) {
         console.log('that was easy');
